8443e90ec286032b280b0e095f6c991623a34d9e
laiwei <laiwei0329@163.com>
2024-03-10 16:51:13 +0800

    添加性别标签模型

--- TextRandJob.py
+++ TextRandJob.py
@@ -13,16 +13,18 @@ class TextRandJob(SparkSessionBase):
 
     def start(self):
         # XXX 大数据分析代码
-        sql = "show databases"
+        # sql = "SELECT * FROM profile_tags.tbl_basic_tags"
         hc = HiveContext(self.spark.sparkContext)
-        res = hc.sql(sql)
+        # res = hc.sql(sql)
+        # res.show()
+
         # 将DataFrame保存为JSON格式的文件
         # res.write.format("json").mode("overwrite").save("test")
         # 将DataFrame保存为hdfs上的csv格式的文件
         # res.write.csv("sample_file.csv", header=True)
         # 将DataFrame保存为本地的csv格式的文件
         # res.toPandas().to_csv("sample_file.csv", header=True)
-        res.show()
+
         # b_df = hc.table('business')
         # b_df.limit(10).show()
 
