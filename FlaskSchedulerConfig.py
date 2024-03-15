3605dff765e390213d65d376c9c8c2d5e181571c
laiwei <laiwei0329@163.com>
2024-03-15 14:23:01 +0800

    增加调度任务

--- FlaskSchedulerConfig.py
+++ FlaskSchedulerConfig.py
@@ -1,5 +1,13 @@
 from models.match.DoGenderTag import DoGenderTag
 from models.match.DoJobTag import DoJobTag
+from models.match.DoPoliticalFaceTag import DoPoliticalFaceTag
+from models.match.DoMarriageTag import DoMarriageTag
+from models.match.DoNationalityTag import DoNationalityTag
+from models.match.DoIsBlackListTag import DoIsBlackListTag
+from models.mining.DoUsgTag import DoUsgTag
+from models.mining.DoRfmTag import DoRfmTag
+from models.mining.DoPsmTag import DoPsmTag
+from models.mining.DoRfeTag import DoRfeTag
 from flask_apscheduler.auth import HTTPBasicAuth
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 
@@ -9,8 +17,8 @@ class Config(object):
         # interval定时执行（从start_date到end_date，间隔20s，包含首尾）
         # func也可以写字符串形式，例如：'App.tasks.DatabaseTask:send_ding_test'
         # {
-        #     'id': 'job2',
-        #     'func': DoGenderTag.start(),
+        #     'id': '',
+        #     'func': DoGenderTag.start,
         #     'trigger': 'interval',
         #     'start_date': '2021-01-27 13:31:00',
         #     'end_date': '2021-01-27 13:33:00',
@@ -18,14 +26,25 @@ class Config(object):
         #     'replace_existing': True  # 重新执行程序时，会将jobStore中的任务替换掉
         # },
         # date一次执行
+        # {
+        #     'id': '',
+        #     'func': DoGenderTag.start,
+        #     'trigger': 'date',
+        #     'run_date': '2024-03-12 11:00:00',
+        #     'replace_existing': True
+        # },
+        # cron式定时调度，类似linux的crontab
         {
             'id': 'do_gender_tag',
             'func': DoGenderTag.start,
-            'trigger': 'date',
-            'run_date': '2024-03-12 11:00:00',
+            'trigger': 'cron',
+            'month': '3',
+            'day': '12',
+            'hour': '11',
+            'minute': '5',
+            'second': '0',
             'replace_existing': True
         },
-        # cron式定时调度，类似linux的crontab
         {
             'id': 'do_job_tag',
             'func': DoJobTag.start,
@@ -36,8 +55,216 @@ class Config(object):
             'minute': '5',
             'second': '0',
             'replace_existing': True
-        }
-
+        },
+        {
+            'id': 'do_isBlackList_tag',
+            'func': DoIsBlackListTag.start,
+            'trigger': 'cron',
+            'month': '3',
+            'day': '12',
+            'hour': '11',
+            'minute': '5',
+            'second': '0',
+            'replace_existing': True
+        },
+        {
+            'id': 'do_marriage_tag',
+            'func': DoMarriageTag.start,
+            'trigger': 'cron',
+            'month': '3',
+            'day': '12',
+            'hour': '11',
+            'minute': '5',
+            'second': '0',
+            'replace_existing': True
+        },
+        {
+            'id': 'do_nationality_tag',
+            'func': DoNationalityTag.start,
+            'trigger': 'cron',
+            'month': '3',
+            'day': '12',
+            'hour': '11',
+            'minute': '5',
+            'second': '0',
+            'replace_existing': True
+        },
+        {
+            'id': 'do_politicalFace_tag',
+            'func': DoPoliticalFaceTag.start,
+            'trigger': 'cron',
+            'month': '3',
+            'day': '12',
+            'hour': '11',
+            'minute': '5',
+            'second': '0',
+            'replace_existing': True
+        },
+        {
+            'id': 'do_psm_tag',
+            'func': DoPsmTag.start,
+            'trigger': 'cron',
+            'month': '3',
+            'day': '12',
+            'hour': '11',
+            'minute': '5',
+            'second': '0',
+            'replace_existing': True
+        },
+        {
+            'id': 'do_rfe_tag',
+            'func': DoRfeTag.start,
+            'trigger': 'cron',
+            'month': '3',
+            'day': '12',
+            'hour': '11',
+            'minute': '5',
+            'second': '0',
+            'replace_existing': True
+        },
+        {
+            'id': 'do_rfm_tag',
+            'func': DoRfmTag.start,
+            'trigger': 'cron',
+            'month': '3',
+            'day': '12',
+            'hour': '11',
+            'minute': '5',
+            'second': '0',
+            'replace_existing': True
+        },
+        {
+            'id': 'do_usg_tag',
+            'func': DoUsgTag.start,
+            'trigger': 'cron',
+            'month': '3',
+            'day': '12',
+            'hour': '11',
+            'minute': '5',
+            'second': '0',
+            'replace_existing': True
+        },
+        # {
+        #     'id': 'do_job_tag',
+        #     'func': DoJobTag.start,
+        #     'trigger': 'cron',
+        #     'month': '3',
+        #     'day': '12',
+        #     'hour': '11',
+        #     'minute': '5',
+        #     'second': '0',
+        #     'replace_existing': True
+        # },
+        # {
+        #     'id': 'do_job_tag',
+        #     'func': DoJobTag.start,
+        #     'trigger': 'cron',
+        #     'month': '3',
+        #     'day': '12',
+        #     'hour': '11',
+        #     'minute': '5',
+        #     'second': '0',
+        #     'replace_existing': True
+        # },
+        # {
+        #     'id': 'do_job_tag',
+        #     'func': DoJobTag.start,
+        #     'trigger': 'cron',
+        #     'month': '3',
+        #     'day': '12',
+        #     'hour': '11',
+        #     'minute': '5',
+        #     'second': '0',
+        #     'replace_existing': True
+        # },
+        # {
+        #     'id': 'do_job_tag',
+        #     'func': DoJobTag.start,
+        #     'trigger': 'cron',
+        #     'month': '3',
+        #     'day': '12',
+        #     'hour': '11',
+        #     'minute': '5',
+        #     'second': '0',
+        #     'replace_existing': True
+        # },
+        # {
+        #     'id': 'do_job_tag',
+        #     'func': DoJobTag.start,
+        #     'trigger': 'cron',
+        #     'month': '3',
+        #     'day': '12',
+        #     'hour': '11',
+        #     'minute': '5',
+        #     'second': '0',
+        #     'replace_existing': True
+        # },
+        # {
+        #     'id': 'do_job_tag',
+        #     'func': DoJobTag.start,
+        #     'trigger': 'cron',
+        #     'month': '3',
+        #     'day': '12',
+        #     'hour': '11',
+        #     'minute': '5',
+        #     'second': '0',
+        #     'replace_existing': True
+        # },
+        # {
+        #     'id': 'do_job_tag',
+        #     'func': DoJobTag.start,
+        #     'trigger': 'cron',
+        #     'month': '3',
+        #     'day': '12',
+        #     'hour': '11',
+        #     'minute': '5',
+        #     'second': '0',
+        #     'replace_existing': True
+        # },
+        # {
+        #     'id': 'do_job_tag',
+        #     'func': DoJobTag.start,
+        #     'trigger': 'cron',
+        #     'month': '3',
+        #     'day': '12',
+        #     'hour': '11',
+        #     'minute': '5',
+        #     'second': '0',
+        #     'replace_existing': True
+        # },
+        # {
+        #     'id': 'do_job_tag',
+        #     'func': DoJobTag.start,
+        #     'trigger': 'cron',
+        #     'month': '3',
+        #     'day': '12',
+        #     'hour': '11',
+        #     'minute': '5',
+        #     'second': '0',
+        #     'replace_existing': True
+        # },
+        # {
+        #     'id': 'do_job_tag',
+        #     'func': DoJobTag.start,
+        #     'trigger': 'cron',
+        #     'month': '3',
+        #     'day': '12',
+        #     'hour': '11',
+        #     'minute': '5',
+        #     'second': '0',
+        #     'replace_existing': True
+        # },
+        # {
+        #     'id': 'do_job_tag',
+        #     'func': DoJobTag.start,
+        #     'trigger': 'cron',
+        #     'month': '3',
+        #     'day': '12',
+        #     'hour': '11',
+        #     'minute': '5',
+        #     'second': '0',
+        #     'replace_existing': True
+        # },
     ]
 
     # 存储定时任务（默认是存储在内存中）
@@ -53,5 +280,5 @@ class Config(object):
     # auth验证。默认是关闭的，
     # SCHEDULER_AUTH = HTTPBasicAuth()
     # 设置定时任务的执行器（默认是最大执行数量为10的线程池）
-    SCHEDULER_EXECUTORS = {'default': {'type': 'threadpool', 'max_workers': 10}}
+    SCHEDULER_EXECUTORS = {'default': {'type': 'threadpool', 'max_workers': 30}}
     # 另外flask-apscheduler内有日志记录器。name为apscheduler.scheduler和apscheduler.executors.default。如果需要保存日志，则需要对此日志记录器进行配置
